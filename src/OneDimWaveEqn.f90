b2a04a131ef63278ab787cb3ddead4aa00c9fcf0
davidho95 <davidho95@gmail.com>
2017-02-06 15:53:28 +0000

    Implemented intial conditions in main program

--- src/OneDimWaveEqn.f90
+++ src/OneDimWaveEqn.f90
@@ -33,16 +33,20 @@
 program OneDimWaveEqn
 
   use Parameters
+  use WaveModule
 
   implicit none
+
+  integer, dimension(NUM_GLL, NUM_SPEC_EL) :: i_bool
+  real(dp), dimension(NUM_GLOBAL_POINTS) :: global_points, rho, mu, displ, vel,&
+    accel
 
 ! Index variables
   integer timestep
 
-  call Setup_mesh()
+  call Setup_mesh(i_bool, rho, mu)
 
-  ! call Inital_conditions()
-
+  call Initial_conditions(displ, vel)
 
   ! do timestep = 1, NUM_TIMESTEPS
   !   call Increment_system()
@@ -50,52 +54,3 @@ program OneDimWaveEqn
   ! enddo
 
 end program OneDimWaveEqn
-
-!===============================================================================
-! Subroutine to set up the global mesh and local ot global numbering
-!===============================================================================
-subroutine Setup_mesh()
-
-  use Parameters
-
-  implicit none
-
-  integer, parameter :: NUM_GLOBAL_POINTS = NUM_SPEC_EL * (NUM_GLL - 1) + 1
-  integer i_spec, i_gll, i_glob
-  real(dp), dimension(0:NUM_SPEC_EL) :: anchor_points
-  real(dp), dimension(NUM_GLL) :: gll_points, gll_weights
-  real(dp), dimension(NUM_GLOBAL_POINTS) :: global_points, rho, mu
-  integer, dimension(NUM_GLL, NUM_SPEC_EL) :: i_bool
-
-  ! GLL points and weights
-  call zwgljd(gll_points,gll_weights,NUM_GLL,0.0_dp,0.0_dp)
-  if(mod(NUM_GLL,2) /= 0) gll_points((NUM_GLL-1)/2+1) = 0.0_dp
-
-  ! Setup anchor points
-  do i_spec = 0, NUM_SPEC_EL
-    anchor_points(i_spec) = dble(i_spec) * LENGTH / dble(NUM_SPEC_EL)
-  enddo
-
-  ! Local to global numbering
-  i_glob = 1
-  do i_spec = 1,NUM_SPEC_EL
-    do i_gll = 1,NUM_GLL
-      if(i_gll > 1) i_glob = i_glob+1
-      i_bool(i_gll,i_spec) = i_glob
-    enddo
-  enddo
-
-! Compute global gridpoint position
-  do i_spec = 1,NUM_SPEC_EL
-    do i_gll = 1,NUM_GLL
-      i_glob = i_bool(i_gll,i_spec)
-      global_points(i_glob) = 0.5*(1.-gll_points(i_gll))*anchor_points(i_spec)&
-        +0.5*(1.+gll_points(i_gll))*anchor_points(i_spec - 1)
-    enddo
-  enddo
-
-  ! Set mesh properties
-  rho = density_fn(global_points)
-  mu = rigidity_fn(global_points)
-
-end subroutine Setup_mesh
