27440fcbc1dd196a6613b4d41dbef408836bf80f
davidho95 <davidho95@gmail.com>
2017-02-23 23:24:06 +0000

    Test program for sine wave

--- src/TwoDimWaveEqn.f90
+++ src/TwoDimWaveEqn.f90
@@ -3,7 +3,7 @@
 !
 ! TwoDimWaveEqn.f90
 !
-! Solves the wave equation on a (1 + 1) dimensional pseudo-Riemannian manifold
+! Solves the wave equation on a (2 + 1) dimensional pseudo-Riemannian manifold
 !
 ! David Ho (2017)
 ! 
@@ -32,10 +32,46 @@
 !===============================================================================
 program TwoDimWaveEqn
 
-  use TwoDimWaveModule
+  use MeshModule
+  use WaveModule
 
   implicit none
 
-  call setup_mesh_2d()
+  type(mesh) :: my_mesh
+  real(dp), allocatable :: displ(:), vel(:), accel(:)
+  real(dp) delta_t
+  integer, parameter :: NUM_TIME_STEPS = 5000
+  integer i_step, ispecx, ispecy, ispec, inode, jnode, iglob
+  integer :: output_step = 25
 
+  character(len=43) :: output_path = "/home/davidho/WaveEqCurvedSpacetime/output/"
+  character(len=50) snapshot_file
+
+  call initialise_mesh(my_mesh, (/5, 5/), 3)
+
+  call initial_conditions(my_mesh, displ, vel, accel)
+
+  delta_t = estimate_timestep(my_mesh)
+
+  do i_step = 1, NUM_TIME_STEPS
+    call increment_system(my_mesh, displ, vel, accel, 1d-3)
+    if (mod(i_step, output_step) == 0) then
+      write(snapshot_file, "('snapshot',i5.5)") i_step
+      open(unit=10, file=output_path//snapshot_file, action="write", status="unknown")
+
+      do ispecx = 1,my_mesh%num_spec_el(1)
+        do inode = 1,my_mesh%num_gll
+          do ispecy = 1,my_mesh%num_spec_el(2)
+            ispec = (ispecx-1)*my_mesh%num_spec_el(2) + ispecy
+            do jnode = 1,my_mesh%num_gll
+              iglob = my_mesh%i_bool(inode,jnode,ispec)
+              write(10,*) my_mesh%nodes(inode,jnode,ispec,1),my_mesh%nodes(inode,jnode,ispec,2),displ(iglob)
+            end do
+          end do
+          write(10,*)
+        end do
+      end do
+      close(10)
+    endif
+  enddo
 end program TwoDimWaveEqn
